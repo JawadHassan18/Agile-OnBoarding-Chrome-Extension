0e14148fc51dc490627dbdc11765c69c8642c1e5
hasanjafri5720 <77610649+hasanjafri5720@users.noreply.github.com>
2021-11-08 14:06:58 +0500

    Add files via upload

---
+++ chromeextension/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.hello = void 0;
+var world = "world";
+function hello(world) {
+    return "Hello " + world + "! ";
+}
+exports.hello = hello;
+//# sourceMappingURL=index.js.map
